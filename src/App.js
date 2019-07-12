e72039bf729d3f2108991e80d46a2effcd95a127
jbgreulich <jessica@greulich.me>
2019-07-12 17:21:31 -0400

    random color bg complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,57 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
 
+import { Button } from './Button';
+
 class App extends Component {
+  constructor(props) {
+  	super(props);
+    this.state = { color: [256, 256, 256] };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a,b) => a+b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.backgroundColor = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random()*256));
+    }
+    return random;
+  }
+
+  handleClick() {
+    this.setState({
+      color: this.chooseColor()
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1> a random color generator app</h1>
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}>a random color generator app
+        </h1>
+        <Button light={this.isLight()} onClick={this.handleClick} />
       </div>
     );
   }
